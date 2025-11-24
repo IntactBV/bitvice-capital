aa0dfb2e1fea3b63c2df7ebb35a9f512086f33d7
BBG <gabi@bitvice.ro>
2025-11-24 16:21:13 +0200

    refactor(tv): :memo: log raw message

--- app/api/tradingview/orion-15/route.ts
+++ app/api/tradingview/orion-15/route.ts
@@ -3,7 +3,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   try {
-    console.log('Received data for orion-15 strategy:', request.body);
+    const rawBody = await request.text();
+    console.log('Raw body from TradingView:', rawBody);
+
     const body = await request.json();
 
     console.log('Body:', body);
